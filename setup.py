ad609e9390b4dec9b92d98a15ed337408f2a6799
Henrique Bisetto <132086713+hbisetto@users.noreply.github.com>
2026-01-22 11:13:43 -0300

    add files #3 (#4)
    closes #3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+            #x.y.z
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Henrique Bisetto",
+    packages=find_packages(),
+)
