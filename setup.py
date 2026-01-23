6ea2d65b2c3c6b9acfbbdd2dcea68898a47043ca
Henrique Bisetto <hbisetto@yahoo.com.br>
2026-01-23 10:06:18 -0300

    "turned into installable #9"

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+            #x.y.z
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Henrique Bisetto",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
+)
